dc0eb7a0b2d14d48f3518b7ce4773ea2b56b38ac
diiivan <dms.ivanov@mail.ru>
2022-12-19 16:21:33 +0300

    Fixes

--- venv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python3 -m venv venv &&
-source venv/bin/activate &&
-pip install -r requirements.txt --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.4.1/constraints-3.10.txt"
